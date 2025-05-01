34dbe0448a8346df8ad8a52b2cbd1e433a37057b
bleepster <bleepster@earll.net>
2025-05-01 21:07:31 +0800

    [dilemma] move caps lock from combo to a layer/tap key

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/custom/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/custom/keymap.c
@@ -89,6 +89,12 @@ enum dilemma_keymap_layers {
 // right half - layer 1 tap
 #define RT_1SPC LT(1, KC_SPC)
 
+// left half - layer 5 tap
+#define LT_5CAPS LT(5, KC_CAPS)
+
+// right half - layer 5 tap
+#define RT_5CAPS LT(5, KC_CAPS)
+
 // left control + left shift
 #define CTRLSHFT LCTL(KC_LSFT)
 
@@ -104,7 +110,7 @@ enum dilemma_keymap_layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_split_3x5_3(
-    MO(LAYER_MISC),  KC_W,  KC_E,  KC_R, LT_3T,       RT_3Y,   KC_U,  KC_I,  KC_O,    MO(LAYER_MISC),
+          LT_5CAPS,  KC_W,  KC_E,  KC_R, LT_3T,       RT_3Y,   KC_U,  KC_I,  KC_O,    RT_5CAPS,
              LH0_1, LH0_2, LH0_3, LH0_4, LT_2G,       RT_2H,   RH0_1, RH0_2, RH0_3,   RH0_4,
               KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,       KC_N,    KC_M,  KC_Q,  KC_COMM, KC_DOT,
                      KC_HYPR, LT_4TAB, LT_1ENT,       RT_1SPC, RH_5,  KC_MEH
@@ -154,7 +160,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 // combos
 const uint16_t PROGMEM combo_backspace[] = {KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM combo_escape[] = {KC_W, KC_E, COMBO_END};
-const uint16_t PROGMEM combo_capslock[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM combo_uparrow[] = {KC_Q, KC_COMM, COMBO_END};
 const uint16_t PROGMEM combo_downarrow[] = {KC_M, KC_Q, COMBO_END};
 const uint16_t PROGMEM combo_rightarrow[] = {KC_C, KC_V, COMBO_END};
@@ -163,7 +168,6 @@ const uint16_t PROGMEM combo_leftarrow[] = {KC_X, KC_C, COMBO_END};
 combo_t key_combos[] = {
     COMBO(combo_backspace, KC_BSPC),
     COMBO(combo_escape, KC_ESC),
-    COMBO(combo_capslock, KC_CAPS),
     COMBO(combo_uparrow, KC_UP),
     COMBO(combo_downarrow, KC_DOWN),
     COMBO(combo_rightarrow, KC_RIGHT),
